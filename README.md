1962b56d0652088aabb6a1aaab2f159b8a1e8957
mryux <mryux2004@gmail.com>
2021-07-07 22:34:18 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iiu16n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iiu16n)
---
+++ src/app/products.ts
@@ -0,0 +1,40 @@
+export class Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.price = 0;
+    this.description = '';
+  }
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 701,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: 'abc'
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
